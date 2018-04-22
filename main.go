b5db211728f1a9467e11b53f63d66931c695c49e
Matthew Roseman <mrr77@case.edu>
2018-04-22 01:21:12 -0400

    Added List of TODO comments

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	// TODO start up discord bot
+	// TODO listen for poll command
+	// TODO on poll command start a poll and listen for results
+	// TODO when enough people have voted to decide a result, share the result
+}
